870e4990467a7d577305365b6780c03e1e422f7b
Daniel Paulino <danielp1469@gmail.com>
2020-02-21 10:59:46 -0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,13 @@ public class Main {
         Quadrado q1 = new Quadrado();
         q1.lado = 10.0;
 
+        Quadrado q2 = new Quadrado();
+        q2.lado = 5.0;
+
         System.out.println(q1.calcularArea());
         System.out.println(q1.calcularPerimetro());
 
+        System.out.println(q2.calcularArea());
     }
 
 }
